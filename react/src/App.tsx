626676c54c08140d8fefd5b0525bb4cca19d5d94
Kyle <Smurflo2@gmail.com>
2022-12-07 13:12:14 -0700

    Start Home page

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Home from "./components/Home";
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
                 <h1>Heyoooo</h1>
             </header>
+            <Home />
+            a?????
         </div>
     );
 }
