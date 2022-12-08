1dbb897200d8999f40bef2cd7cde5ec6b0baf109
icculus479 <cyrus.irani@gmail.com>
2022-12-07 23:19:36 -0700

    Update Vote.sol
    Fun new changes!

--- solidity/contracts/Vote.sol
+++ solidity/contracts/Vote.sol
@@ -8,131 +8,177 @@ import "hardhat/console.sol";
 
 contract ETHDenverVotingMVP is Ownable {
 
-uint public proposalID;
-uint public LatestProposalID;
+uint256 public proposalID;
+uint256 public cycleID;
+uint256 public LatestProposalID;
 
-enum proposalStatus{OPEN, CLOSED}
+// 4 different statuses for a proposal cycle
+enum cycleStatus{OPEN, DISCUSSION, VOTING, CLOSED}
 
+struct Cycle{
+    string cycleTitle;
+    uint256 cycleDistrict;
+    cycleStatus status;
+    uint cycleExpiration;
+}
+mapping(uint256 => Cycle) public CycleDirectory;
+
+//**** Proposal Setup ****//
 struct Proposal {
     address creator;
-    proposalStatus status;
     uint256 votingDistrict;
     uint256 yVotes;
     uint256 nVotes;
+    string title;
     string description;
-    uint voteExpiration;
-    mapping(address => VoterRecord ) VoterRecords;
+    //mapping(address => VoterRecord ) VoterRecords;
 }
 
-struct VoterRecord {
-        bool hasRegistered;
+struct VoterRecord { 
         bool hasVoted;
         bool vote;
         // This comes in later with QV...
         //uint256 weight;
 }
-
-struct VoterRegistration {
-    uint256 voterDistrict;
-    bool registered;
-}
-
-struct ProposalToken {
-    uint propTokenID;
-
-}
-
 mapping(uint256 => Proposal) public Proposals;
+Proposal[] ProposalArray; 
+
+
+// Voter Registrations - Establishing District and if that address has been registered 
+struct VoterRegistration {
+    bool registered;
+    uint256 voterDistrict;
+}
+
 mapping(address => VoterRegistration) public VoterRegistrations;
+
+// Mapping created to store user addresses and their corresponding balances
+    mapping (address => uint256) public VoteTokenBalances;
+
+// Address array ...
+   address[] VoterAddresses;
 
 // Voter must register with the application in order to do anything
 // Check the voter hasn't registered previously, then set the values in the struct for that user
 function registerVoter(uint256 _votingDistrict) public {
 
     require(_votingDistrict != 0, "Invalid Voting District");
-    VoterRegistration storage currentVoter = VoterRegistrations[msg.sender];
-    require(currentVoter.registered == false, "Voter has already Registered");
-    currentVoter.registered = true;
-    currentVoter.voterDistrict = _votingDistrict;
+    VoterRegistration storage currentVoterReg = VoterRegistrations[msg.sender];
+    require(currentVoterReg.registered == false, "Voter has already Registered");
+    currentVoterReg.registered = true;
+    currentVoterReg.voterDistrict = _votingDistrict;
+
+    VoteTokenBalances[msg.sender] = 100; 
+    VoterAddresses.push(msg.sender);
 }
 
-function createProposal(string calldata _description, uint _voteExpiration) public {
- 
-    //Pulling the District information in from the sender, as only somebody in a district should be able to create a Proposal for that district
-    uint256 _votingDistrict = VoterRegistrations[msg.sender].voterDistrict;
-    require(_voteExpiration > 0, "Open Voting Period must not be 0");
-    require(bytes(_description).length != 0, "Please fill out the proposal description");
- 
+function createProposalCycle(string calldata _title, uint256 _cycleDistrict, uint _cycleExpiration) public onlyOwner() {
+
     //Get current Timestamp
     uint deployDate = block.timestamp;
     console.log(deployDate);
+    //Default is 0, 1st iteration will set it to 1, and so on with new cycles
+    cycleID++;
+    
+    Cycle storage currentCycle = CycleDirectory[cycleID];
+    currentCycle.cycleTitle = _title;
+    currentCycle.cycleDistrict = _cycleDistrict;
+    currentCycle.status = cycleStatus.OPEN;
+
+    //Timestamp is in seconds. Assuming VoteExpiration will be submitted in Days, we will convert everythign into seconds
+    currentCycle.cycleExpiration = deployDate + (_cycleExpiration * 24 * 60 * 60); 
+    console.log(currentCycle.cycleExpiration);
+    
+}
+
+function cycleRegistration() public {
+
+    //Require to make sure the Cycle is open
+    require(getCycleStatus(cycleID) == cycleStatus.OPEN,"Sorry, the current Cycle is not Open");
+    // Require user has registered
+    require(getVoterRegistrationStatus() == true, "User must register before joining a proposal");
+    // Require that the User's distict matches the current Cycle District
+    require(CycleDirectory[cycleID].cycleDistrict == VoterRegistrations[msg.sender].voterDistrict,"User must reside in the district to register to this cycle");
+    
+    
+    // Okay so here we need to give them the tokens, voting and discussion //
+}
+
+function createProposal(string calldata _title, string calldata _description, uint256 _votingDistrict) public {
+    
+    //Require to make sure the Cycle is open
+    //require(getCycleStatus(cycleID) == cycleStatus.OPEN,"Sorry, the current Cycle is not Open");
+    //Require to make sure you can only create a Proposal which you registered to
+    require(VoterRegistrations[msg.sender].voterDistrict == _votingDistrict,"You can only make a proposal for your district");
+    require(bytes(_description).length != 0, "Please fill out the proposal description");
 
     //Default is 0, Starts with 1
     proposalID++;
     
     Proposal storage currentProp = Proposals[proposalID];
     currentProp.creator = msg.sender;
-    currentProp.status = proposalStatus.OPEN;
     currentProp.votingDistrict = _votingDistrict;
-    //Timestamp is in seconds. Assuming VoteExpiration is in Days, converting to Seconds
-    currentProp.voteExpiration = deployDate + (_voteExpiration * 24 * 60 * 60); 
-    console.log(currentProp.voteExpiration);
+    currentProp.title = _title;
     currentProp.description = _description;
+
+    // The logic missing here is how do we associate the Proposal to a District - just haven't go that far 
     
 }
 
+function getAllProposals() public view returns (Proposal[] memory){
 
-function proposalRegistration(uint _proposalID) public {
-
-    require(getProposalStatus(_proposalID) == proposalStatus.OPEN, "Sorry, Proposal is Closed");
-    require(getVoterRegistrationStatus() == true, "User must register before joining a proposal");
-    require(Proposals[_proposalID].votingDistrict == VoterRegistrations[msg.sender].voterDistrict, "User must reside in the district of the proposal to register");
-    require(Proposals[_proposalID].VoterRecords[msg.sender].hasRegistered == false, "User has already registered for this proposal");
-    
-    Proposals[_proposalID].VoterRecords[msg.sender].hasRegistered == true;
-    
-    // Here, I need to assign voting tokens to the user for this specific proposal.  I need to make a new mapping for balances, but it can't be one token 
-    // for everything, so I think it needs to be a mapping like (address => Another Struct), where the other Struct has an ID (which would be the proposalID
-    // and then another uint which would be the token value the user gets.  This is a bit confusing so im stopping here at this point
-
-
+    Proposal[] memory toReturn = new Proposal[](proposalID);
+    for (uint i = proposalID; i > 0; i--) {
+        toReturn[i-1] = Proposals[i];
+    }
+    return toReturn;
 }
 
-function voting(uint _proposalID) public {
-    //Check to make sure the user is registered
-    require(Proposals[_proposalID].VoterRecords[msg.sender].hasRegistered == true, "You are not registered to vote for this Proposal");
-    require(getProposalStatus(_proposalID) == proposalStatus.OPEN, "Sorry, Proposal is Closed for Voting");
-    //Then we need to make a check if they have already voted (but is it okay if they vote once, and then want to go back and vote more , QV style?
-    // I think so, so maybe we need to get the QV voting in here until they run out of those tokens that were given during the proposalRegistration
+function voting(uint _proposalID, uint _numOfVotes, bool isY) public {
 
-    // We then apply the tokens as votes for the proposal and update the voting mappings (this may not be correct as coded for multiple votes at this time, just one I think would work now
+     uint cost = _numOfVotes * _numOfVotes;
+     require(VoteTokenBalances[msg.sender] >= cost, "User does not have enough Voting Tokens for this vote");
+
+     if (isY){
+      Proposals[_proposalID].yVotes += _numOfVotes;
+     }
+     else {
+      Proposals[_proposalID].nVotes += _numOfVotes;
+     }
+     // We need to subtract cost here from msg.sender balance
+     VoteTokenBalances[msg.sender] -= cost; 
 }
 
-//Then we need the function to fetch / display the results of each proposal
-//function results(uint _proposalID public returns)
+function resetTokenBalances() public onlyOwner() {
 
-
-
+    for (uint i = 0; i < VoterAddresses.length; i++) {
+       VoteTokenBalances[VoterAddresses[i]] = 100;
+    }
+}
 
 // Function Checks for Requires //
 
-// Function to check if the Proposal is currently open.
-function getProposalStatus(uint _proposalID) public returns (proposalStatus) {
+//Function Check for User Registration
+function getVoterRegistrationStatus() public view returns (bool) {
+    return VoterRegistrations[msg.sender].registered;
+}
+
+//Function Check for Cycle Status - Used by other functions to ensure the Cycle is Open - Will need to adjust for Voting Time later
+function getCycleStatus(uint256 _cycleID) public returns (cycleStatus) {
+
     uint currentTime = block.timestamp;
-    uint expirationTime = Proposals[_proposalID].voteExpiration;
+    Cycle storage currentCycle = CycleDirectory[_cycleID];
+
+    uint expirationTime = currentCycle.cycleExpiration;
     require(expirationTime != 0, "Proposal has not been setup");
 
     if (currentTime < expirationTime) {
-        Proposals[_proposalID].status = proposalStatus.OPEN;
+        currentCycle.status = cycleStatus.OPEN;
     } else {
-        Proposals[_proposalID].status = proposalStatus.CLOSED;
+        currentCycle.status = cycleStatus.CLOSED;
     }
-    return Proposals[_proposalID].status;
-}
 
-//Function to check if user has registered to the application
-function getVoterRegistrationStatus() public view returns (bool) {
-    return VoterRegistrations[msg.sender].registered;
+    return CycleDirectory[_cycleID].status;
 }
 
 }
